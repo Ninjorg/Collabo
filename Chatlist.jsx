a7df20fe7ba787586b250773dc1be87d82cc387c
Ronit Parikh <138072436+Ninjorg@users.noreply.github.com>
2024-07-31 17:28:23 -0700

    Update Chatlist.jsx

--- Chatlist.jsx
+++ Chatlist.jsx
@@ -1,17 +1,59 @@
-import "./ChatList.css"
+import React, { useState } from 'react';
+import './ChatList.css';
 
 const Chatlist = () => {
+    const [addMode, setAddMode] = useState(false);
+
     return (
-        <div className ='chatList'>
-            <div className = "search">
-                <div className = "searchBar">
-                    <img src= "./search.png" alt="" />
-                    <input type= "text" placeholder="search" />
+        <div className='chatList'>
+            <div className='search'>
+                <div className='searchBar'>
+                    <img src="./search.png" alt="Search Icon" />
+                    <input type='text' placeholder='Search' />
                 </div>
-                <img src= "./plus.png" alt="" className= "add" />
+                <img
+                    src={addMode ? "./minus.png" : "./plus.png"}
+                    alt={addMode ? "Minus Icon" : "Plus Icon"}
+                    className='add'
+                    onClick={() => setAddMode(prev => !prev)}
+                />
             </div>
+            <div className= "item" alt="">
+
+                <img src=" ./avatar.png" alt=""/>
+                <div className= "texts">
+                    <span>Charles Dickonson</span>
+                    <p>Big OOGHA</p>
+                </div>
+            </div>
+            <div className= "item" alt="">
+
+                <img src=" ./avatar.png" alt=""/>
+                <div className= "texts">
+                    <span>Charles Dickonson</span>
+                    <p>Big OOGHA</p>
+                </div>
+            </div>
+            <div className= "item" alt="">
+
+                <img src=" ./avatar.png" alt=""/>
+                <div className= "texts">
+                    <span>Charles Dickonson</span>
+                    <p>Big OOGHA</p>
+                </div>
+            </div>
+            
+            <div className= "item" alt="">
+
+                <img src=" ./avatar.png" alt=""/>
+                <div className= "texts">
+                    <span>Charles Dickonson</span>
+                    <p>Big OOGHA</p>
+                </div>
+            </div>
+            
         </div>
-    )
+    );
 }
 
-export default Chatlist
+export default Chatlist;
