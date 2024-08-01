639ac7ea8006ac9688a3fa73cad752f51e398f55
Ronit Parikh <138072436+Ninjorg@users.noreply.github.com>
2024-07-31 18:27:15 -0700

    Update Chat.jsx

--- Chat.jsx
+++ Chat.jsx
@@ -1,7 +1,49 @@
-import "./chat.css"
+import React, { useState } from 'react';
+import "./chat.css";
+import EmojiPicker from "emoji-picker-react";
+
 const Chat = () => {
+    const [open,setOpen] = useState(false)
+    const [text,setText] = useState("")
+
+    const handleEmoji = e => {
+        console.log(e)
+        setText(prev => prev + e.emoji);
+        setOpen(false)
+    }
     return (
-        <div className ='chat'>Chat</div>
+        <div className ='chat'>
+            <div className="top">
+                <div className="user">
+                    <img src="./avatar.png" alt=""/>
+                    <div className="texts">
+                        <span>Timy</span>
+                        <p>My name is Ronit, You have a leaky focet then I'm on it.</p>
+                    </div>
+                </div>
+                <div className="icons">
+                    <img src="./phone.png" alt=""/>
+                    <img src="./video.png" alt=""/>
+                    <img src="./info.png" alt=""/>
+                </div>
+            </div>
+            <div className="center"></div>
+            <div className="bottom">
+                <div className="icon">
+                    <img src="./img.png" alt=""/>
+                    <img src="./camera.png" alt=""/>
+                    <img src="./mic.png" alt=""/>
+                </div>
+                <input type="text" placeholder="Type a message..." value= {text} onChange={(e=>setText(e.target.value))}/>
+                <div className="emoji">
+                    <img src="./emoji.png" alt="" onClick={() => setOpen(prev => !prev)}/>
+                    <div className="picker">
+                        <EmojiPicker open={open} onEmojiClick={handleEmoji}/>
+                    </div>
+                </div>
+                <button className= "sendButton">Send</button>
+            </div>
+        </div>
     )
 }
 
