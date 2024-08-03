67b5fbb621f2d870dc942a4340a5a14c981933f8
Ronit Parikh <138072436+Ninjorg@users.noreply.github.com>
2024-08-03 10:49:00 -0700

    Update App.jsx

--- App.jsx
+++ App.jsx
@@ -1,15 +1,20 @@
+import React, { useState } from "react";
 import Chat from "./components/chat/Chat";
 import List from "./components/list/List";
 import Detail from "./components/detail/Detail";
+import "./App.css";
 
 const App = () => {
+  const [selectedChat, setSelectedChat] = useState(null);
+  const [selectedDetail, setSelectedDetail] = useState(null);
+
   return (
     <div className='container'>
-      <List/>
-      <Chat/>
-      <Detail/>
+      <List setSelectedChat={setSelectedChat} />
+      <Chat selectedChat={selectedChat} />
+      <Detail selectedDetail={selectedDetail} />
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
