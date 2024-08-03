79b792c5aecee4a06dd8c2cdaf82f00e9a4aec50
Ronit Parikh <138072436+Ninjorg@users.noreply.github.com>
2024-08-03 12:05:53 -0700

    Add files via upload

---
+++ Login.jsx
@@ -0,0 +1,21 @@
+
+import "./login.css";
+
+const Login = () => {
+    return (
+        <>
+            <div className="homePage">
+                <h1>Welcome to Collabo</h1>
+                <p>Collabo is an educational tool designed to help students collaborate effectively. Whether you're working on group projects, sharing resources, or discussing ideas, Collabo provides the platform you need to succeed together.</p>
+                <div className="login">
+                    <div className="item">Login Item 1</div>
+                    <div className="seperator"></div>
+                    <div className="item">Login Item 2</div>
+                </div>
+            </div>
+
+        </>
+    );
+}
+
+export default Login;
